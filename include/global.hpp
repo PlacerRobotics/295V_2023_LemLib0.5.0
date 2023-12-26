e3b1189ab1a47bc8bac9ce914d36915115527b91
Pavan <pavankumar28052007@gmail.com>
2023-12-25 21:01:50 -0800

    12/25

---
+++ include/global.hpp
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "lemlib/api.hpp"
+
+// Catapult motor
+extern pros::Motor catapultMotor;
+
+// Intake motor
+extern pros::Motor intakeMotor;
+
+// Distance sensor for catapult
+extern pros::Distance cataDistance;
+
+// Wings
+extern pros::ADIDigitalOut wing1;
+extern pros::ADIDigitalOut wing2;
+
+// Intake pistons
+extern pros::ADIDigitalOut intakePiston1;
+extern pros::ADIDigitalOut intakePiston2;
